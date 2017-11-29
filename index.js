6f5133d44af2378673bcaa4d7ae3f658a8b39927
liset valdes <lvaldes@techlaunch.io>
2017-11-29 18:46:23 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+console.log("ready for javascript")
+
+var oper1 = 24+0
+var oper2 =24-0
+var oper3= 24*1
+var oper4= 24/1
+
+//console.log(oper1)
+
+//console.log(5 !==3)
+var op1 = 6 % 3
+var op2 = 10 %2
+var op3 = 5 % 2
+
+//console.log(op2 !== op3)//should say true
+//console.log(op3 < op2)//should say false
+
+//to calculate can be use wil all op
+//console.log(op1)
+
+var string1 = "Test Grades as follows"
+var string2 = "Student: 96"
+var string3 = "Comment: \"None\""
+
+//console.log(string1+"\n \t"+string2+"\n \t"+string3)
+//exercise 4
+var numbers = [0,10,20,30,40,50,60,70,80,90,100]
+
+//console.log(numbers[2+4])
+//need to ask
+var me = {
+    first: "Liset",
+    last: "Valdes",
+    age: "29",
+    email: "lvaldes@teclaunch.io",
+    phone: "xxx-xxx-xxxx"
+}
+    
+
+//console.log('Hi my name is ${me.first}')  
+
+
+//exercise 5
+
+var num = 1
+while(num < 10){
+    if(num % 2 == 0){
+
+        //console.log(num)
+    }
+num ++
+}
+
+
+
+//exersice 6
+
+//#1for(var i=1; i<=10; i++){
+    if(i%2 == 1){
+ //console.log(i)
+    }
+//}
+
+for(var i=1; i<=10; i+=2){
+    console.log(i)
+}
